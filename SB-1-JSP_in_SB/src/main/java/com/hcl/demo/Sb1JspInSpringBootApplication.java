8ef87bd8c5e2d51747b6b40a35d59237ca4e4efa
Vishal Singh <vishal267s@gmail.com>
2025-06-13 14:18:57 +0530

    commitment

---
+++ SB-1-JSP_in_SB/src/main/java/com/hcl/demo/Sb1JspInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb1JspInSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb1JspInSpringBootApplication.class, args);
+	}
+
+}
---
+++ SB-10-REST-Global-Exception-handling/src/main/java/com/hcl/demo/advice/MyControllerAdvice.java
@@ -0,0 +1,41 @@
+package com.hcl.demo.advice;
+
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.hcl.demo.exception.EmptyInputException;
+
+@ControllerAdvice
+public class MyControllerAdvice extends ResponseEntityExceptionHandler{
+
+	@ExceptionHandler(EmptyInputException.class)
+	public ResponseEntity<String> handleEmptyInput(EmptyInputException e) {
+		
+		return new ResponseEntity<String>("Input field is empty, please look into it.",HttpStatus.BAD_REQUEST);
+	}
+	@ExceptionHandler(NoSuchElementException.class)
+	public ResponseEntity<String> handleNoSuchElementInDB(NoSuchElementException e) {
+		
+		return new ResponseEntity<String>("No Such Element is present in DB",HttpStatus.NOT_FOUND);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+			HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		return new ResponseEntity<Object>("Please Change your HTTP method",HttpStatus.METHOD_NOT_ALLOWED);
+	}
+	
+	
+}
