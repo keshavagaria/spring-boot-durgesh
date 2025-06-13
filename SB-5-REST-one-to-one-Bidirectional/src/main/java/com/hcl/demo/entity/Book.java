88134d4569f7c0b5517be1475e000722938e12e8
Vishal Singh <vishal267s@gmail.com>
2025-06-13 16:48:17 +0530

    cum

---
+++ SB-5-REST-one-to-one-Bidirectional/src/main/java/com/hcl/demo/entity/Book.java
@@ -0,0 +1,71 @@
+package com.hcl.demo.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+@Table(name = "THE_BOOKS")
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String title;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "THE_AUTHOR_ID")
+	@JsonManagedReference
+	private Author author;
+	
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Book(int id, String title, Author author) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
+	}
+	
+	
+
+}
